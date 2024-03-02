fccc5f18149ef79afbf550d89ead37942edb7d51
Claus Alberto Bienemann <claus.a.bienemann@gmail.com>
2024-03-02 18:02:48 -0300

    Update sth.py

--- sth.py
+++ sth.py
@@ -11,18 +11,17 @@ def get_attribute_data(entity, lastN=None):
         url = f"http://{ip}:8666/STH/v1/contextEntities/type/dt/id/urn:ngsi-ld:wine:001/attributes/{entity}?lastN={lastN}"
     else:
         url = f"http://{ip}:1026/v2/entities/urn:ngsi-ld:wine:001/attrs/{entity}"
-
+    result = None
     try:
         response = requests.get(url, headers=HEADERS)
         response.raise_for_status()
-
-        data = response.json()
-        return data
+        result = response.json()
     except requests.HTTPError as http_err:
-        error = f"Erro HTTP ao obter dados: {http_err}"
+        result = f"Erro HTTP ao obter dados: {http_err}"
     except Exception as err:
-        error = f"Erro ao obter dados: {err}"
-    return error
+        result = f"Erro ao obter dados: {err}"
+    finally:
+        return result
 
 if __name__ == '__main__':
     attribute_data = get_attribute_data("tempext")
